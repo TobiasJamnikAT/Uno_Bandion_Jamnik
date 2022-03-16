f76e4949595b99236921b67722b7e6d1e3c7c575
Tobias Jamnik <jamtod19@htl-kaindorf.at>
2022-03-16 15:50:26 +0100

    Initial commit

---
+++ app/src/main/java/com/example/uno_bandion_jamnik/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.uno_bandion_jamnik;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
